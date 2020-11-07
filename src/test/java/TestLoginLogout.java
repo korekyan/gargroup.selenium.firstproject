35d9df04b38492e769dd95537dc698708e3e1ed0
korekyan <74025244+korekyan@users.noreply.github.com>
2020-11-07 03:01:09 +0200

    Add files via upload
    add files

---
+++ src/test/java/TestLoginLogout.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestLoginLogout {
+    private WebDriver driver;
+    @BeforeMethod
+    public void setup(){
+        driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/index.php/login");
+        driver.manage().window().maximize();
+    }
+
+    @Test(priority = 1, description = "login and check elements")
+    public void testLogin(){
+        //define elements
+        WebElement loginElement = driver.findElement(By.id("txtUsername"));
+        WebElement passwElement = driver.findElement(By.xpath("//input[@name='txtPassword']"));
+        WebElement submitButton = driver.findElement(By.xpath("//input[@type='submit']"));
+
+        //check and login
+        Assert.assertTrue(driver.getCurrentUrl().contains("login"));
+        loginElement.sendKeys("Admin");
+        passwElement.sendKeys("admin123");
+        submitButton.click();
+
+        // check result
+        WebElement marketplaceMark = driver.findElement(By.id("MP_link"));
+        Assert.assertEquals(marketplaceMark.getAttribute("value"), "Marketplace");
+        Assert.assertTrue(driver.getCurrentUrl().contains("dashboard"));
+
+        //back to previous page
+        driver.navigate().back();
+        Assert.assertTrue(driver.getCurrentUrl().contains("login"));
+        driver.navigate().refresh();
+        WebElement messWarn = driver.findElement(By.xpath("//div[@class='message warning']"));
+        Assert.assertEquals(messWarn.getText(), "Invalid Request");
+    }
+
+   @AfterMethod
+    public void teardown(){
+        driver.quit();}
+    }
